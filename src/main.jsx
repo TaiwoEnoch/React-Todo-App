55c5324a0a03423d0635c21952693f3241218907
TaiwoEnoch <taiwoenoch0@gmail.com>
2023-05-01 15:01:37 +0100

    Make some changes in my code base

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import TodoApp from '@/components/TodoApp';
+import '@/styles/app.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const domContainer = document.getElementById('root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TodoApp />
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
